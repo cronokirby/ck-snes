a4a384e5f66b737c7fbcbd460f3987311266a09b
Lucas Meier <lucas@cronokirby.com>
2025-10-11 11:26:26 -0700

    Define Bus abstraction

---
+++ lib/ck-snes-core/src/bus.rs
@@ -0,0 +1,16 @@
+/// Represents the current status of the bus.
+///
+/// By sharing mutable access to this status, components can communicate.
+///
+/// We don't represent the bus as pins directly, but instead as distinct states.
+#[derive(Clone, Copy)]
+pub enum Bus {
+    /// A read request is on the bus.
+    AskRead(u32),
+    /// A write request is on the bus.
+    AskWrite(u32, u8),
+    /// The response to a read request is on the bus.
+    ReplyRead(u8),
+    /// The response to a write request is on the bus.
+    ReplyWrite,
+}
