742f2486665a93399dc4c0f770e61ba7fa355b9e
Lucas Meier <lucas@cronokirby.com>
2026-03-28 15:27:42 -0700

    Implement basic decompilation framework
    We don't handle any instructions besides the first Super Mario World instruction,
    but hey, only up from here.

--- asm/ck-snes-asm/src/lib.rs
+++ asm/ck-snes-asm/src/lib.rs
@@ -1,3 +1,51 @@
-pub fn assemble(_input: &str) -> Vec<u8> {
-    Vec::new()
+use std::io;
+
+/// This holds all of the data for a SNES game, including code, music, and graphics.
+pub struct Rom {
+    bytes: Vec<u8>,
+}
+
+impl Rom {
+    /// Construct a new ROM, given a blob of bytes.
+    pub fn new(bytes: Vec<u8>) -> Self {
+        Self { bytes }
+    }
+
+    /// Decompile this ROM, writing it to some sink.
+    ///
+    /// This takes in anything implementing [`io::Write`]
+    pub fn decompile(&self, writer: &mut impl io::Write) -> anyhow::Result<()> {
+        let entry: u16 = 0xfffc;
+        let mut i = self.u16(entry);
+        loop {
+            match self.u8(i) {
+                0x78 => writeln!(writer, "  sei")?,
+                x => todo!("unknown instr {x:02X} at {i:04X}"),
+            }
+            i += 1;
+        }
+    }
+
+    fn translate(&self, at: u16) -> usize {
+        at as usize - 0x8000
+    }
+
+    fn u8(&self, at: u16) -> u8 {
+        self.bytes[self.translate(at)]
+    }
+
+    /// Read a u16 from a particular location, panicking upon failure.
+    ///
+    /// Panicking is usually the right choice, in the context of decompilation,
+    /// because the ROM is malformed if this is going to fail, or there's a bug
+    /// in our program. Either way, just terminating the program is probably for
+    /// the better.
+    fn u16(&self, at: u16) -> u16 {
+        let addr = self.translate(at);
+        u16::from_le_bytes(
+            self.bytes[addr..addr + 2]
+                .try_into()
+                .expect("expected 2 bytes"),
+        )
+    }
 }
--- asm/ck-snes-asm/src/main.rs
+++ asm/ck-snes-asm/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("ck-snes-asm");
+use std::path::{Path, PathBuf};
+
+use ck_snes_asm::Rom;
+use pico_args::Arguments;
+
+fn main() -> anyhow::Result<()> {
+    let mut args = Arguments::from_env();
+    match args.subcommand()?.as_deref() {
+        None => {
+            print_help();
+            Ok(())
+        }
+        Some("decompile") => decompile(args),
+        Some(other) => {
+            println!("Unknown sub-command: {other}");
+            print_help();
+            Ok(())
+        }
+    }
+}
+
+fn print_help() {
+    println!("TODO: add help information");
+}
+
+fn decompile(mut args: Arguments) -> anyhow::Result<()> {
+    let input: PathBuf = args.free_from_str()?;
+    let output: PathBuf = args.free_from_str()?;
+    run_decompile(&input, &output)
+}
+
+fn run_decompile(input: &Path, output: &Path) -> anyhow::Result<()> {
+    let bytes = std::fs::read(input)?;
+    Rom::new(bytes).decompile(&mut std::fs::File::create(output)?)
 }
